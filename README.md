c86d014e6487c53a60d21a6d43baec97b1dabbce
Taichi Uragami <backpaper0@gmail.com>
2015-01-21 22:37:21 +0900

    セットアップするスクリプト書いた

--- README.md
+++ README.md
@@ -1,8 +1,14 @@
-# dotfiles
+dotfiles
+==================================================
 
-諸々の設定。
+## Install
 
-## ライセンス
+```
+git clone git@github.com:backpaper0/dotfiles.git ~/dotfiles
+sh ~/dotfiles/setup.sh
+```
+
+## License
 
 [Apache License Version 2.0](http://www.apache.org/licenses/LICENSE-2.0.txt)
 
---
+++ setup.sh
@@ -0,0 +1,19 @@
+#!/bin/sh
+
+DOTFILES=(gitconfig vimrc zshrc)
+
+for file in $DOTFILES
+do
+  ln -s ~/dotfiles/.$file ~/.$file
+done
+
+if [ ! -e ~/.vim/bundle ]
+then
+  mkdir -p ~/.vim/bundle
+fi
+
+if [ ! -e ~/.vim/bundle/neobundle.vim ]
+then
+  git clone git@github.com:Shougo/neobundle.vim.git ~/.vim/bundle/neobundle.vim
+fi
+
